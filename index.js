39d2b6cb4b063462a9993f68395ac0ca6c5d70c5
thdevss <pichet.suesat@gmail.com>
2023-12-21 13:49:21 +0700

    [*] change if cond.

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
     const jsonData = req.body;
+    let topic = 'pair'
     if (req.query.ref === 'crypto') {
-        broker.publish('crypto', JSON.stringify(jsonData));
-    } else {
-        broker.publish('pair', JSON.stringify(jsonData));
+        topic = 'crypto'
     }
+    broker.publish(topic, JSON.stringify(jsonData));
+    // TODO:: save to db
     res.json({ 
         message: 'published',
         data: jsonData
